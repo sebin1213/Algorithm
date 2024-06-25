7c147d1c716330e8589a09db8ca5a970241042a6
hope1213 <1213hope@gmail.com>
2024-06-25 18:29:44 +0900

    Create 지도 자동구축.java

---
+++ Softeer/지도 자동구축.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int count = 2;
+        for (int i = 0; i < n ; i++){
+            count += Math.pow(2,i);
+        }
+        System.out.println(count*count);
+    }
+}
